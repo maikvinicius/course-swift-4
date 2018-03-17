7a01a5b5ef6b9e8deb9f2086be0f8623e56bb0e2
Maik Vinicius <maikmv.mv@gmail.com>
2018-03-17 20:16:56 -0300

    Add Playground

---
+++ Swift1 - Comentarios e Variaveis.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+/*
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+O comando abaixo serve para imprimir no console.
+Use print(variavel) que será impressa.
+*/
+*/
+// Swift: Lançada em 2014, atualmente na versão 4.0
+
+var name = "Maik Vinicius"
+print(name)
+
+// Variáveis
+
+var age = 21 // Mutable
+var 🐶 = "Derek"
+var 👶🏻 = 1
+var 👶🏽 = 2
+var sum = 👶🏻 + 👶🏽
+print("O nome do meu cachorro é", 🐶)
+
+var loop = "1 Infinite loop"
+var myFatherAge = 66
+
+// Constantes
+
+let pi = 3.1415926 // Unmutable
+let monthsInYear = 12
