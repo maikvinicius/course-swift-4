e651a7f11b9fa345f5aa480603576a78bbc7e70a
Maik Vinicius <maikmv.mv@gmail.com>
2018-03-17 18:11:34 -0300

    Project IMC - Create LauchScreen and FirstController with layout and constrains

--- IMC/IMC/ViewController.swift
+++ IMC/IMC/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
 
 }
 
