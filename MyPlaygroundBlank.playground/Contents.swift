59afcbe1b5e1b0de302275f0eb124f5420a29286
Maik Vinicius <maikmv.mv@gmail.com>
2018-03-17 17:24:03 -0300

    Working with Playground

---
+++ MyPlaygroundBlank.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var name = "Maik Vinicius"
+var number = 7 + 5
+
+print("A soma de 7 com 5 é", number)
+
+var age = 18
+
+var a = 0
+
+for i in 1...15{
+    a = a + i
+}
+print(a)
