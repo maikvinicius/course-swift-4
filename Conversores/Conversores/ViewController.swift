4380be6bba4b1e4aac23d94b3c347ece0ed8f961
Maik Vinicius <maikmv.mv@gmail.com>
2018-03-17 20:00:13 -0300

    Project Conversores - Create project

---
+++ Conversores/Conversores/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Maik Vinicius on 17/03/18.
+//  Copyright © 2018 Maik Vinicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var lblResult: UILabel!
+    @IBOutlet weak var lblResultUnit: UILabel!
+    @IBOutlet weak var btUnitOne: UIButton!
+    @IBOutlet weak var btUnitTwo: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
+    
+}
+
